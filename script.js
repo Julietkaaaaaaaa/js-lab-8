bb9e5a467a2dc1b375953c2ffc9ef26e60c85de5
Julietkaaaaaaaa <162048724+Julietkaaaaaaaa@users.noreply.github.com>
2026-05-11 14:23:44 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,17 +1,10 @@
+
 const burgerBtn = document.getElementById("burgerBtn");
 const navMenu = document.getElementById("navMenu");
-
-let timer = setInterval(nextSlide, 3000);
-
-function resetTimer() {
-    clearInterval(timer); 
-    timer = setInterval(nextSlide, 3000); 
-}
 
 
 burgerBtn.addEventListener("click", () => {
     navMenu.classList.toggle("show");
-    resetTimer();
 });
 
 const slides = document.getElementById("slides");
@@ -22,44 +15,51 @@ const nextBtn = document.getElementById("nextBtn");
 
 let currentIndex = 0;
 
+//Управління таймером
+let timer = setInterval(nextSlide, 3000); // Створюємо таймер ОДИН раз
+
+function resetTimer() {
+    clearInterval(timer); 
+    timer = setInterval(nextSlide, 3000); 
+}
+
+//Функції каруселі
 function updateCarousel() {
     slides.style.transform = `translateX(-${currentIndex * 100}%)`;
-
-    dots.forEach(dot => {
-        dot.classList.remove("active");
-    });
-
+    dots.forEach(dot => dot.classList.remove("active"));
     dots[currentIndex].classList.add("active");
 }
 
 function nextSlide() {
     currentIndex++;
-
     if (currentIndex >= slide.length) {
         currentIndex = 0;
     }
-
     updateCarousel();
 }
 
 function prevSlide() {
     currentIndex--;
-
     if (currentIndex < 0) {
         currentIndex = slide.length - 1;
     }
-
     updateCarousel();
 }
 
-nextBtn.addEventListener("click", nextSlide);
-prevBtn.addEventListener("click", prevSlide);
+nextBtn.addEventListener("click", () => {
+    nextSlide();
+    resetTimer();
+});
 
-setInterval(nextSlide, 3000);
+prevBtn.addEventListener("click", () => {
+    prevSlide();
+    resetTimer(); 
+});
 
 dots.forEach((dot, index) => {
     dot.addEventListener("click", () => {
         currentIndex = index;
         updateCarousel();
+        resetTimer(); 
     });
 });
